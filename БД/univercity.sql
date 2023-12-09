479f60ac552b013f0a5deba560a048dddc068298
makarova1507ana <103330304+makarova1507ana@users.noreply.github.com>
2023-12-09 17:24:08 +0400

    Update univercity.sql

--- БД/univercity.sql
+++ БД/univercity.sql
@@ -1,46 +1,8 @@
--- Создание таблицы "Students"
-CREATE TABLE Students (
-    student_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    email VARCHAR(100),
-    course INT,
-    group_id INT
-);
 
--- Добавление записей в таблицу "Students"
-INSERT INTO Students (first_name, last_name, email, course, group_id) VALUES
-    ('Иван', 'Иванов', 'ivan@example.com', 2, 1),
-    ('Елена', 'Петрова', 'elena@example.com', 3, 1),
-    ('Алексей', 'Сидоров', 'alex@example.com', 2, 2),
-    ('Ольга', 'Смирнова', 'olga@example.com', 1, 2),
-    ('Дмитрий', 'Козлов', 'dmitriy@example.com', 3, 1),
-    ('Татьяна', 'Федорова', 'tanya@example.com', 2, 2),
-    ('Павел', 'Морозов', 'pavel@example.com', 1, 1),
-    ('Мария', 'Васильева', 'maria@example.com', 3, 2),
-    ('Александра', 'Павлова', 'sasha@example.com', 1, 1),
-    ('Сергей', 'Кузнецов', 'sergei@example.com', 2, 2);
 
--- Создание таблицы "Groups"
-CREATE TABLE Groups (
-    group_id INT PRIMARY KEY AUTO_INCREMENT,
-    group_name VARCHAR(100),
-    professor_id INT
-);
+Create database if not exists university;
 
--- Добавление записей в таблицу "Groups"
-INSERT INTO Groups (group_name, professor_id) VALUES
-    ('Группа 1', 1),
-    ('Группа 2', 2),
-    ('Группа 3', 3),
-    ('Группа 4', 1),
-    ('Группа 5', 2),
-    ('Группа 6', 3),
-    ('Группа 7', 1),
-    ('Группа 8', 2),
-    ('Группа 9', 3),
-    ('Группа 10', 1);
-
+use university;
 -- Создание таблицы "Professors"
 CREATE TABLE Professors (
     professor_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -56,12 +18,38 @@ INSERT INTO Professors (first_name, last_name, email, department) VALUES
     ('Елена', 'Сидорова', 'elena_sidorova@example.com', 'Математика'),
     ('Алексей', 'Иванов', 'alex_ivanov@example.com', 'Физика'),
     ('Ольга', 'Смирнова', 'olga_smirnova@example.com', 'Иностранные языки');
+    
+
+
+-- Создание таблицы "groups_students"
+CREATE TABLE if not exists groups_students (
+    group_id INT PRIMARY KEY AUTO_INCREMENT,
+    group_name VARCHAR(100),
+    professor_id INT,
+	FOREIGN KEY(professor_id) REFERENCES Professors(professor_id)
+
+);
+
+-- Добавление записей в таблицу "Groups"
+INSERT INTO groups_students (group_name, professor_id) VALUES
+    ('Группа 1', 1),
+    ('Группа 2', 2),
+    ('Группа 3', 3),
+    ('Группа 4', 1),
+    ('Группа 5', 2),
+    ('Группа 6', 3),
+    ('Группа 7', 1),
+    ('Группа 8', 2),
+    ('Группа 9', 3),
+    ('Группа 10', 1);
+    
 
 -- Создание таблицы "Courses"
 CREATE TABLE Courses (
     course_id INT PRIMARY KEY AUTO_INCREMENT,
     course_name VARCHAR(100),
-    professor_id INT
+	FOREIGN KEY(professor_id) REFERENCES Professors(professor_id)
+
 );
 
 -- Добавление записей в таблицу "Courses"
@@ -76,14 +64,46 @@ INSERT INTO Courses (course_name, professor_id) VALUES
     ('Биология', 4),
     ('Химия', 1),
     ('География', 2);
+    
+    -- Создание таблицы "Students"
+CREATE TABLE if not exists Students (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    course INT,
+    group_id INT,
+	FOREIGN KEY(course) REFERENCES courses(course_id),
+
+    FOREIGN KEY(group_id) REFERENCES groups_students(groups_id)
+);
+
+-- Добавление записей в таблицу "Students"
+INSERT INTO Students (first_name, last_name, email, course, group_id) VALUES
+    ('Иван', 'Иванов', 'ivan@example.com', 2, 1),
+    ('Елена', 'Петрова', 'elena@example.com', 3, 1),
+    ('Алексей', 'Сидоров', 'alex@example.com', 2, 2),
+    ('Ольга', 'Смирнова', 'olga@example.com', 1, 2),
+    ('Дмитрий', 'Козлов', 'dmitriy@example.com', 3, 1),
+    ('Татьяна', 'Федорова', 'tanya@example.com', 2, 2),
+    ('Павел', 'Морозов', 'pavel@example.com', 1, 1),
+    ('Мария', 'Васильева', 'maria@example.com', 3, 2),
+    ('Александра', 'Павлова', 'sasha@example.com', 1, 1),
+    ('Сергей', 'Кузнецов', 'sergei@example.com', 2, 2);
+
+
+
+
+
+
 
 -- Создание таблицы "Schedule"
 CREATE TABLE Schedule (
     schedule_id INT PRIMARY KEY AUTO_INCREMENT,
     day_of_week VARCHAR(20),
     time TIME,
-    course_id INT,
-    group_id INT
+	FOREIGN KEY(course) REFERENCES courses(course_id),
+    FOREIGN KEY(group_id) REFERENCES groups_students(groups_id)
 );
 
 -- Добавление записей в таблицу "Schedule"
@@ -107,4 +127,12 @@ INSERT INTO Schedule (day_of_week, time, course_id, group_id) VALUES
 -- Получить информацию о студентах, их курсах и дате их занятий:
 -- Получить все уникальные курсы, в которых зарегистрированы студенты:
 -- Получить список всех курсов и соответствующих преподавателей, включая информацию о студентах, изучающих эти курсы:
+
+
+
+--  примеры с UNION 
+
+
+
+
 
